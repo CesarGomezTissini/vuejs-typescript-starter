42d146c2cab1141ae1e171b9f9c275b5bb17dfa2
Cesar Gomez <cesarg@tissini.com>
2020-06-30 18:08:42 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  plugins: ['vuetify'],
+
+  extends: [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended', // we added this line
+    '@vue/prettier',
+  ],
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vuetify/no-deprecated-classes': 'error',
+    'vuetify/grid-unknown-attributes': 'error',
+    'vuetify/no-legacy-grid': 'error',
+  },
+
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier',
+    '@vue/typescript',
+  ],
+}
